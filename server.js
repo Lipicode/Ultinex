9a59d66adc495c7d3ded24eb4cde9c4f27f2f6f5
LiprodCodez <turtle.script.dev@gmail.com>
2025-01-13 17:51:12 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.use('/proxy', createProxyMiddleware({
+  changeOrigin: true,
+  target: '', // dynamically set in the query
+  router: (req) => req.query.url,
+  onProxyReq(proxyReq, req) {
+    if (req.query.url) proxyReq.setHeader('Host', new URL(req.query.url).host);
+  },
+}));
+
+app.listen(PORT, () => console.log(`Ultinex running at http://localhost:${PORT}`));
